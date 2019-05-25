6dc4abaa02e5c6307d76e52a664f5302cb7d2fcd
elorenzen <eric.lorenzen@gmail.com>
2019-05-25 11:58:52 -0600

    Changed import filepaths for components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TopNavbar from './TopNavbar';
-import SecondNavbar from './SecondNavbar';
-import Sidebar from './Sidebar';
-import Post from './Post';
-import Buttons from './Buttons';
+import TopNavbar from './components/TopNavbar';
+import SecondNavbar from './components/SecondNavbar';
+import Sidebar from './components/Sidebar';
+import Post from './components/Post';
+import Buttons from './components/Buttons';
 
 
 // Create React nav component
