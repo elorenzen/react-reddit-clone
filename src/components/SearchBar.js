1d40fb7b9cb4c979b1c57723ac3e1fb1cd032d38
elorenzen <eric.lorenzen@gmail.com>
2019-05-26 08:52:45 -0600

    Added input change event handler and added onChange attribute to input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
     render() {
         return (
             <div className="nav navbar-nav">
                 <form className="nav-item">
-                    <input className="form-control mr-sm-2" id='nav-search' type="search" placeholder="Search Reddit" aria-label="Search" />
+                    <input onChange={this.onInputChange} className="form-control mr-sm-2" id='nav-search' type="search" placeholder="Search Reddit" aria-label="Search" />
                 </form>
             </div>
         )
