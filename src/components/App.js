02fae487254dc941a254c6f0aec79bbc0e462299
elorenzen <eric.lorenzen@gmail.com>
2019-05-26 09:00:14 -0600

    Created component file to be imported into index.js

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import TopNavbar from './TopNavbar';
+import SecondNavbar from './SecondNavbar';
+import Sidebar from './Sidebar';
+import Post from './Post';
+import Buttons from './Buttons';
+
+const App = () => {
+    return (
+        <div>
+            <TopNavbar />
+            <SecondNavbar />
+            <div className="reddit_container" style={{maxWidth: '100%'}}>
+                <div className="post_container">
+                    <div className="post_list">
+                        <Post> 
+                            <Buttons />
+                        </Post>
+                        <Post> 
+                            <Buttons />
+                        </Post>
+                        <Post> 
+                            <Buttons />
+                        </Post>    
+                    </div>
+                </div>
+                <Sidebar />
+            </div>
+        </div>
+    ) 
+};
+
+export default App;
