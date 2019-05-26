5816308dd3faf2f4f8e539ceb12cd4ec024569fd
elorenzen <eric.lorenzen@gmail.com>
2019-05-26 09:01:08 -0600

    Removed App component to new file in /components and imported App component to index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,9 @@
 // Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TopNavbar from './components/TopNavbar';
-import SecondNavbar from './components/SecondNavbar';
-import Sidebar from './components/Sidebar';
-import Post from './components/Post';
-import Buttons from './components/Buttons';
 
+import App from './components/App';
 
-// Create React nav component
-const App = () => {
-    return (
-        <div>
-            <TopNavbar />
-            <SecondNavbar />
-            <div className="reddit_container" style={{maxWidth: '100%'}}>
-                <div className="post_container">
-                    <div className="post_list">
-                        <Post> 
-                            <Buttons />
-                        </Post>
-                        <Post> 
-                            <Buttons />
-                        </Post>
-                        <Post> 
-                            <Buttons />
-                        </Post>    
-                    </div>
-                </div>
-                <Sidebar />
-            </div>
-        </div>
-    ) 
-};
 
 // Take React component and show it on screen
 ReactDOM.render(
