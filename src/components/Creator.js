589640c66008c9d3db6dc3e718d81865ffa7e3cd
elorenzen <eric.lorenzen@gmail.com>
2019-05-26 14:36:23 -0600

    Passed props into event handlers found in inputs

--- src/components/Creator.js
+++ src/components/Creator.js
@@ -1,20 +1,43 @@
 import React from 'react';
 
 class Creator extends React.Component {
+    state = { 
+        title: '',
+        text: ''
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.titleClick(this.state.title);
+        this.props.textClick(this.state.text);
+    }
 
     render() {
         return (
             <div style={{backgroundColor: 'white', marginBottom: '15px', borderRadius: '5px'}}>
                 <div style={{margin: '16px'}}>
-                    <form action="/reddit-posts" method="POST">
+                    <form>
                         <div>
-                            <textarea type="text" className="new_post_title_textarea" placeholder="Title"  style={{height: '39px'}}></textarea>
+                            <input 
+                                value={this.state.title}
+                                onChange={event => this.setState({ title: event.target.value })}
+                                type="text" 
+                                className="new_post_title_textarea" 
+                                placeholder="Title"  
+                                style={{height: '39px'}} 
+                            />
                         </div>
                         <div className="new_post_text_container">
-                            <input type="text" className="new_post_text_editor" placeholder="Text (optional)" />
+                            <input
+                                value={this.state.text} 
+                                onChange={event => this.setState({ text: event.target.value })}
+                                type="text" 
+                                className="new_post_text_editor" 
+                                placeholder="Text (optional)" 
+                            />
                         </div>
                         <div className="new_post_button_container">
-                            <button className="new_post_post_button">
+                            <button onClick={event => this.onFormSubmit(event)} className="new_post_post_button">
                                 post
                             </button>
                         </div>
